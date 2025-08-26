ed22d63756897ddcc1c17790f135e0954e057e92
EduardoCasarrubias <L25550594@chihuahua2.tecnm.mx>
2025-08-25 22:40:09 -0600

    Add files via upload

---
+++ EVA1-practica 1- hola mundo/EVA1PRACTICA1HOLAMUNDO.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1.practica.pkg1.hola.mundo;
+
+
+public class EVA1PRACTICA1HOLAMUNDO {
+
+
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+        System.out.println("FUNDAMENTOS DE PROGRAMACION");
+    }
+    
+   
+}
