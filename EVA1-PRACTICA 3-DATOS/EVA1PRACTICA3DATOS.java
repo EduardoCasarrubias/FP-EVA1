96e7b67c55e0ae7503ff559141aa22cf05e68ac7
EduardoCasarrubias <L25550594@chihuahua2.tecnm.mx>
2025-08-31 20:34:03 -0600

    Add files via upload

---
+++ EVA1-PRACTICA 3-DATOS/EVA1PRACTICA3DATOS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1.practica.pkg3.datos;
+
+/**
+ *
+ * @author ecasa
+ */
+public class EVA1PRACTICA3DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("NOMBRE DEL ESTUDIANTE: ");
+        System.out.println("EDUARDO CASARRUBIAS GONZALEZ");
+        System.out.print("NUMERO DE CONTROL: ");
+        System.out.println(25550594);
+        System.out.print("EDAD: ");
+        System.out.println(17);
+    }
+    
+}
