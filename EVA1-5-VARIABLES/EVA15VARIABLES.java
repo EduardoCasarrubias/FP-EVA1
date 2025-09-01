048cf99bbe721a62931aed37daaa8487096fcb81
EduardoCasarrubias <L25550594@chihuahua2.tecnm.mx>
2025-08-31 20:35:18 -0600

    Add files via upload

---
+++ EVA1-5-VARIABLES/EVA15VARIABLES.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1.pkg5.variables;
+
+/**
+ *
+ * @author ecasa
+ */
+public class EVA15VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // declarar variables 
+        //tipo de dato  nombre delidentificador 
+        String nombre;
+        int edad;
+        double salario; 
+        //inicializacion 
+        //= --- > operaador de asignacion
+        nombre ="Eduardo Casarrubias Gonzalez";
+        edad = 17;
+        salario = 2000.40; 
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+    }
+    
+}
