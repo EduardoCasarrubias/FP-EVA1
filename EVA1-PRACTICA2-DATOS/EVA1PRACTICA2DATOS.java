575078c0953ca0b081bfeb74a0c49f138d04ed7a
EduardoCasarrubias <L25550594@chihuahua2.tecnm.mx>
2025-08-31 20:29:46 -0600

    Add files via upload

---
+++ EVA1-PRACTICA2-DATOS/EVA1PRACTICA2DATOS.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1.practica2.datos;
+
+public class EVA1PRACTICA2DATOS {
+
+    
+    public static void main(String[] args) {
+      //;significa fin de la linea
+      //"  " cadena de texto. DATO
+       System.out.println("EDUARDO CASARRUBIAS");
+       //NUMEROS ENTEROS
+       System.out.println(1000);
+       //NUMEROS DECIMALES
+       System.out.println(52.5);
+       //VALORES LOGICOS(VERDADERO O FALSO)
+       //TRUE FALSE 
+       System.out.println(true);
+       //CARACTERES INDIVIDUALES 
+       //cosas distintias
+       System.out.println('c');//caracter
+       System.out.println("c");//cadena de texto
+    }
+    
+}
