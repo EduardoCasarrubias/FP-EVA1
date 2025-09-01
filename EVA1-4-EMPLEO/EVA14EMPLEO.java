b79050832710592815432dce4d4fee87b14c3635
EduardoCasarrubias <L25550594@chihuahua2.tecnm.mx>
2025-08-31 20:34:50 -0600

    Add files via upload

---
+++ EVA1-4-EMPLEO/EVA14EMPLEO.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1.pkg4.empleo;
+
+/**
+ *
+ * @author ecasa
+ */
+public class EVA14EMPLEO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("EMPLEO: ");
+        System.out.println("jugador de futbol soccer");
+        System.out.print("SALARIO: ");
+        System.out.println(1000000);
+        System.out.print("HORAS SEMANALES: ");
+        System.out.println(10);
+        System.out.print("REQUIERE INGLES: ");
+        System.out.println("true");
+    }
+    
+}
