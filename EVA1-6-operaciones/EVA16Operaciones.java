ee7db7fd00be0510a97b2eb36707254ba08fbb08
EduardoCasarrubias <L25550594@chihuahua2.tecnm.mx>
2025-09-03 16:22:34 -0600

    Add files via upload

---
+++ EVA1-6-operaciones/EVA16Operaciones.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1.pkg6.operaciones;
+
+/**
+ *
+ * @author ecasa
+ */
+public class EVA16Operaciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // SECCION DE DECLARACION
+        double area, p, radio; // TODAS SON DOUBLE
+        //ES LO MISMO
+        //BOUBLE PI;
+        //DOUBLE RADIO;
+        //DOUBLE AREA 
+        //INICIACION
+        p=3.1416;
+        radio=10;
+        //OPERACION 
+        area=p*radio*radio;
+        //SALIDA DE DATOS 
+        System.out.println("el area del circulo de radio= 10 es");
+        System.out.println(area);
+        
+    }
+    
+}
