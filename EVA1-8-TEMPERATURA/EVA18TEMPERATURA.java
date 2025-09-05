a82d1431969d098c23d8d54b48b167953ffb888f
EduardoCasarrubias <L25550594@chihuahua2.tecnm.mx>
2025-09-05 16:41:15 -0600

    Add files via upload

---
+++ EVA1-8-TEMPERATURA/EVA18TEMPERATURA.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1.pkg8.temperatura;
+
+/**
+ *
+ * @author ecasa
+ */
+public class EVA18TEMPERATURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double F, C;
+        
+        F= 300;
+        C= (F-32)*5/9;
+        System.out.println("los grados Fahrenheit= 300 a Celsius es= ");
+        System.out.println(C);
+    }
+    
+}
