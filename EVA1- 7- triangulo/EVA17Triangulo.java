64a023b056edfaaedf25ff594eb61f48fbee4df7
EduardoCasarrubias <L25550594@chihuahua2.tecnm.mx>
2025-09-05 16:38:40 -0600

    Add files via upload

---
+++ EVA1- 7- triangulo/EVA17Triangulo.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1.pkg7.triangulo;
+
+/**
+ *
+ * @author ecasa
+ */
+public class EVA17Triangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double area, base, altura;
+        
+        base= 10; 
+        altura= 20;
+        area= base* altura / 2;
+        System.out.println("el area del triangulo es=");
+        System.out.println(area);
+                
+    }
+    
+}
